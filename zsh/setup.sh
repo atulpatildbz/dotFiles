d55919a0d2a1a5daf5f9ca221a35b30b927b497c
Atul Patil <atul.patil@druva.local>
2021-08-02 06:37:04 +0000

    added git clone command for powerlevel10k

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 cp -b .zshrc ~/
 cp -rb .zsh ~/
