2228bf6bfc4b6afe9da15f8a1ecfe4b90b42cbd4
Atul Patil <atulpatildbz@gmail.com>
2022-02-18 22:42:42 +0530

    telescope vertical perview for fuzzy search

--- neovim0.5/nvim/lua/keymaps.lua
+++ neovim0.5/nvim/lua/keymaps.lua
@@ -58,7 +58,7 @@ keymap("n", "<leader>v", ":e $MYVIMRC<cr>", opts)       -- open vimrc
 keymap("n", "<leader>p", ":lua require'telescope.builtin'.builtin{}<CR>", opts)
 keymap("n", "<leader>m", ":lua require'telescope.builtin'.oldfiles{}<CR>", opts)                                -- most recentuly used files
 keymap("n", "<leader>b", ":lua require'telescope.builtin'.buffers{}<CR>", opts)                                 -- find buffer
-keymap("n", "<leader>/", ":lua require'telescope.builtin'.current_buffer_fuzzy_find{}<CR>", opts)               -- find in current buffer       
+keymap("n", "<leader>/", ":lua require'telescope.builtin'.current_buffer_fuzzy_find({layout_strategy='vertical'})<CR>", opts)               -- find in current buffer       
 keymap("n", "<leader>'", ":lua require'telescope.builtin'.marks{}<CR>", opts)                                   -- find bookmarks   
 keymap("n", "<C-p>", ":lua require'telescope.builtin'.git_files({layout_strategy='vertical'})<CR>", opts)       -- git files
 keymap("n", "<leader>rg", ":lua require'telescope.builtin'.live_grep({layout_strategy='vertical'})<CR>", opts)  -- ripgrep like grep through dir 
