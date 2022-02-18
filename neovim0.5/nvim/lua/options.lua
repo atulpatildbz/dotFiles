597665d0a26795f4156498198f0a83d014b77bf9
Atul Patil <atulpatildbz@gmail.com>
2022-02-18 13:09:05 +0530

    moving config file to lua -> 1.options

---
+++ neovim0.5/nvim/lua/options.lua
@@ -0,0 +1,36 @@
+local options = {
+    -- boolean
+    relativenumber = true,
+    nu = true,
+    hidden = true,
+    errorbells = false,
+    expandtab = true,
+    smartindent = true,
+    smartcase = true,
+    ignorecase = true,
+    swapfile = false,
+    backup = false,
+    undofile = true,
+    incsearch = true,
+    cursorline = true,
+    termguicolors = true,                                -- this variable must be enabled for colors to be applied properly
+    -- number
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    -- string
+    mouse = "a",
+    background = "dark",
+    undodir = "~/.nvim/undodir",
+    signcolumn = "yes",
+    clipboard = "unnamed",
+    -- table
+    completeopt = { "menuone", "noinsert", "noselect"},  -- Set completeopt to have a better completion experience
+}
+
+vim.opt.shortmess:append "c"                            -- Avoid showing message extra message when using completion
+
+-- for each key and value in options, set vim.opt[key] to value
+for key, value in pairs(options) do
+    vim.opt[key] = value
+end
