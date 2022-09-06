ad2b6cd883a3144da46f0a97641436abea88a09b
Atul Patil <atulpatildbz@gmail.com>
2022-09-06 10:10:48 +0530

    added print statement

--- neovim0.5/nvim/lua/plugins.lua
+++ neovim0.5/nvim/lua/plugins.lua
@@ -3,6 +3,7 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
+ print("Installing packer at location: " .. install_path)
  PACKER_BOOTSTRAP = fn.system {
    "git",
    "clone",
